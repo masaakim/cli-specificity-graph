661e95fcdc196b2e0a302905cc01967eb6c32aef
Masaaki Morishita <ikasama3.0@gmail.com>
2015-01-04 03:25:22 +0900

    init cli

---
+++ cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var sg = require('./')
+var minimist = require('minimist')
+var argv = minimist(process.argv.slice(2), {
+    boolean: [
+        'help',
+        'versions'
+    ],
+    alias: {
+        h: 'help',
+        V: 'versions'
+    }
+})
+var pkg = require('./package.json')
+
+if (argv.V) console.log(pkg.version)
+
+if (argv.h) {
+    console.log('Usage: acss input-name output-name [options]');
+    console.log('');
+    console.log('Options:');
+    console.log('');
+    console.log('  -c, --compress    use output compression');
+    console.log('  -V, --versions    output the version number');
+    console.log('  -h, --help        output usage information');
+}
+
+if (argv._[0]) {
+    var css = fs.readFileSync(argv._[0], 'utf-8')
+    sg(css)
+}
